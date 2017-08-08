9fa0751571d48e4ecef842659b58040b9aa5be8e
Michael Peter Gower Amiethyst <designatevoid@gmail.com>
2017-08-08 09:34:49 -0700

    Initial checkin of GDX created project.

---
+++ html/src/name/michaelamiethyst/games/mythcreator/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package name.michaelamiethyst.games.mythcreator.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import name.michaelamiethyst.games.mythcreator.GameMain;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener createApplicationListener () {
+                return new GameMain();
+        }
+}
