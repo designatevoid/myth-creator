0184183d22468d634ca90b88cb6927f9618efced
Michael Peter Gower Amiethyst <designatevoid@gmail.com>
2017-08-08 20:34:00 -0700

    Changed sprite to png and added movement to only image.

--- core/src/name/michaelamiethyst/games/mythcreator/GameMain.java
+++ core/src/name/michaelamiethyst/games/mythcreator/GameMain.java
@@ -9,19 +9,27 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class GameMain extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	private int xy = 0;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		String spriteName = "Warhammer40kSprites.png";
+		img = new Texture(spriteName);
 	}
 
+	/**
+	 * <p>Main Game Loop here.</p>
+	 * 
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(img, xy, xy);
+		xy ++;
 		batch.end();
 	}
 	
